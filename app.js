87aa7da9459786a1a82281b51b2c07483b8d5a98
JSpru <jgspruance@gmail.com>
2017-09-21 19:07:43 -0700

    Adding env var for port

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const app = express();
 const users = require('./routes/users');
 
 // Port number
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // CORS middleware
 app.use(cors());
