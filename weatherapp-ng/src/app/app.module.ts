a572948f3ec3660c6a7e3079aa6a71e6734a52c3
Jonathan Spruance <jspruance@inflightcorp.com>
2017-09-21 23:46:40 -0700

    Fixing home pg redirect

--- weatherapp-ng/src/app/app.module.ts
+++ weatherapp-ng/src/app/app.module.ts
@@ -26,8 +26,7 @@ import { AuthGuard } from './guards/auth.guard';
 const appRoutes: Routes = [
   {
     path: '',
-    redirectTo: '/home',
-    pathMatch: 'full'
+    component: HomeComponent
   },
   {
     path: 'home',
