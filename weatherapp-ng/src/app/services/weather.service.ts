e6a3233796086a36aa6b1f60d63d5f2fe2e544f5
Jonathan Spruance <jspruance@inflightcorp.com>
2017-09-23 23:38:06 -0700

    Fix search zip / city input

--- weatherapp-ng/src/app/services/weather.service.ts
+++ weatherapp-ng/src/app/services/weather.service.ts
@@ -6,7 +6,7 @@ import 'rxjs/add/operator/map';
 export class WeatherService {
 
   appid:string = "&appid=fd61eab401147e78b825c2f71cdea941";
-  resturl:string = "http://api.openweathermap.org/data/2.5/weather?q=";
+  resturl:string = "http://api.openweathermap.org/data/2.5/weather";
   iconclass:string;
 
   constructor(public http:Http) { 
@@ -14,7 +14,12 @@ export class WeatherService {
   }
 
   getWeather(location) {
-    let url = this.resturl + location + this.appid;
+    let queryPrefix = "?q=";
+    let isnum = /^\d+$/.test(location);
+    if(isnum === true) {
+      queryPrefix = "?zip=";
+    }
+    let url = this.resturl + queryPrefix + location + this.appid;
     return this.http.get(url).map(res => res.json());
   }
 
